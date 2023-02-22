36d8e3d877dbc6122e99340b9c0b6c1a21610e82
qyerke <70740293+qelephant@users.noreply.github.com>
2023-02-22 11:50:59 +0600

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+		    System.out.println("Vowel"); 
+		}
+		else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+			System.out.println("Consonent");
+		else {
+			System.out.println("wrong alphabet!");
+		}
     }
 }
