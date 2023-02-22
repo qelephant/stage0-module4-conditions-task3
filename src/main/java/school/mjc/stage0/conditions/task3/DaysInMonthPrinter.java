d3b1c05eeaabba274f1327ceeaed162750881b2a
qyerke <70740293+qelephant@users.noreply.github.com>
2023-02-22 11:57:51 +0600

    Update DaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if(mouth > 0){
+        if(mouth==2){
+            System.out.println("28");
+        }
+        else if(mouth%2 == 0) {
+            System.out.println("30");
+        } else if (mouth%2 == 1) {
+            System.out.println("31");
+            }
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
